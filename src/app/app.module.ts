36b4e20b135362a0398e9c9ed0cc353fdbf77e07
Biki101 <thakurbiki52@gmail.com>
2023-12-02 21:59:43 +0545

    added html to newsletter footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { MatIconModule } from '@angular/material/icon';
 import { TopbarModule } from './components/topbar/topbar.module';
-import { TopbarComponent } from './components/topbar/topbar.component';
 import { NavBarComponent } from './components/shared/nav-bar/nav-bar.component';
+import { FooterModule } from './components/footer/footer.module';
 
 @NgModule({
-  declarations: [AppComponent, FooterComponent, NavBarComponent],
-  imports: [BrowserModule, AppRoutingModule, TopbarModule],
+  declarations: [AppComponent, NavBarComponent],
+  imports: [BrowserModule, AppRoutingModule, TopbarModule, FooterModule],
   providers: [],
   bootstrap: [AppComponent],
 })
